039c6065d6588124da1f2aa98c20caf465872654
Alex Jeffers <alex@gluestudio.co.uk>
2026-06-04 08:41:54 +0100

    bunch of fixes

--- wp-content/themes/ligtas/header.php
+++ wp-content/themes/ligtas/header.php
@@ -4,8 +4,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
     <meta name="format-detection" content="telephone=no" />
-    <link rel="stylesheet" type="text/css" href="<?php bloginfo('template_url'); ?>/css/main.min.css">
-    <link rel="stylesheet" type="text/css" href="<?php bloginfo('template_url'); ?>/style.css">
+    <link rel="stylesheet" type="text/css" href="<?php bloginfo('template_url'); ?>/css/main.min.css?v=<?php echo filemtime(get_template_directory() . '/css/main.min.css'); ?>">
+    <link rel="stylesheet" type="text/css" href="<?php bloginfo('template_url'); ?>/style.css?v=<?php echo filemtime(get_template_directory() . '/style.css'); ?>">
 	<link rel="icon" type="image/x-icon" href="<?php bloginfo('template_url'); ?>/images/favicon.png">
     <?php wp_head(); ?>
 </head>
